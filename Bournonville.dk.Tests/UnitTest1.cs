8bea977a3413c720cd333f0ff20c5e7211fd03d6
massidk <e-david@live.dk>
2016-04-28 09:20:35 +0200

    Add project files.

---
+++ Bournonville.dk.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bournonville.dk.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
